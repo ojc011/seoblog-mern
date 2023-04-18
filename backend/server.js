474b6b859f36ecde81d4a8e967888f4c9d4b685d
Oliver C <ojc011@yahoo.com>
2023-04-18 14:51:51 -0500

    Created signup route and controller for auth + signup validator

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // bring routes
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 // app
 const app = express();
@@ -28,7 +29,7 @@ if (process.env.NODE_ENV == 'development') {
 app.use(cors());
 // routes middleware
 app.use('/api', blogRoutes)
-;
+app.use('/api', authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
